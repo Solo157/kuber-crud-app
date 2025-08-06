4ff84870c8dc6e079c3a42fc65bda320727042f4
kirill <kirill.vasyukov@eltex.loc>
2025-08-06 23:03:40 +0700

    Initial commit

---
+++ src/main/java/com/app/kuber_crud_app/KuberCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.kuber_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberCrudAppApplication.class, args);
+	}
+
+}
